f945cd52070b4f4f24a7b00b20cca4b231071711
gentom <gento.m.developer@gmail.com>
2017-11-20 18:37:10 +0900

    Taylor expansion in C

--- Analysis/Series/Taylor/taylor.c
+++ Analysis/Series/Taylor/taylor.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-double taylor(int terms, double seq, double input){
+double taylor(int term, double seq, double input){
     double sum;
     sum = seq;
-    for (int t = 1; t < terms; t++){
+    for (int t = 1; t < term; t++){
         seq *= input/t; //recursion
         sum += seq;
     }
